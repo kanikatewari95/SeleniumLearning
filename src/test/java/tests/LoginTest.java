1af99cb0d1e4a61566d4ac41e1e62e1655af6323
kanikatewari95 <92709080+kanikatewari95@users.noreply.github.com>
2025-04-17 18:09:35 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -6,16 +6,4 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
-    LoginPage loginPage = new LoginPage(driver);
-
-    @Test
-    public void testValidLogin(){
-
-        loginPage.enterUsername("admin@yourstore.com");
-        loginPage.enterPassword("admin");
-        loginPage.clickLogin();
-
-        System.out.println("Title of the page is : " + driver.getTitle());
-
-    }
 }
